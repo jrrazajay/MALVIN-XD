2bef87682ab8546fbe3b55292530b2530887350b
jrrazajay <elevenrazafimaharo@gmail.com>
2025-10-05 00:57:28 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~LxImVAwb#UydHnPQiggywV5tlNMcK45nWc-aUONcYTEy6RmLBeYk18f82915e0b8aec9",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "330698540655", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
-  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "" //put your country timeZone....leave blank if u don't know.
 };
